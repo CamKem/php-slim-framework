d2268fa59e853d858803888f98d371d49f4bef32
Cameron Kemshal-Bell <cam@panr.app>
2024-02-21 18:56:09 +1100

    New controller class object, to hold base methods & Functionality of other controller objects.

---
+++ Core/Controller.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Core;
+
+class Controller
+{
+
+    protected string $controller;
+    protected string $namespace;
+
+    public function __construct()
+    {
+        // we will use this to define some common methods that we will use in our controllers
+    }
+
+    public function extractNamespace($controller)
+    {
+        // first remove the class name by taking the last part of the string away backwards to the first backslash
+        $namespace = substr($controller, 0, strrpos($controller, '\\'));
+
+        // if the namespace is now just \Controllers\ we will remove the Controllers part
+        $namespace = str_replace('Controllers\\', '', $namespace);
+
+        // add the first slash
+        $namespace .= '/';
+
+        // add the controller name
+        $this->controller = $controller;
+
+        // add the namespace to the controller
+        $this->namespace = $namespace;
+    }
+
+}
