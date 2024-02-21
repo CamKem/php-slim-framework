68a84149c4edb8bf81cbf816722a39d691fa8104
Cameron Kemshal-Bell <cam@panr.app>
2024-02-22 00:33:52 +1100

    Updated route definition to work with the new improved setup

--- routes.php
+++ routes.php
@@ -1,22 +1,25 @@
 <?php
 
-use Controllers\Notes\NotesController;
-use Core\Router;
+use app\Controllers\About;
+use app\Controllers\ContactController;
+use app\Controllers\HomeController;
+use app\Controllers\Notes\NotesController;
+use app\Core\Route;
 
-$router->get('/', 'controllers/index.php');
-$router->get('/about', 'controllers/About.php');
-$router->get('/contact', 'controllers/Contact.php');
+Route::get('/', HomeController::class)->name('home');
+Route::get('/about', About::class)->name('about');
+Route::get('/contact', ContactController::class)->name('contact');
 
-$router->get('/notes', [NotesController::class, 'index']);
-$router->get('/note', [NotesController::class, 'show']);
-$router->delete('/note', [NotesController::class, 'destroy']);
-$router->get('/notes/create', [NotesController::class, 'create']);
-$router->post('/notes', [NotesController::class, 'store']);
+Route::get('/notes', [NotesController::class, 'index'])->name('notes.index');
+Route::get('/note', [NotesController::class, 'show'])->name('notes.show');
+Route::delete('/note', [NotesController::class, 'destroy'])->name('notes.destroy');
+Route::get('/notes/create', [NotesController::class, 'create'])->name('notes.create');
+Route::post('/notes', [NotesController::class, 'store'])->name('notes.store');
 
 // TODO: make the calls static, so that the router is a singleton.
 // so we don't have multiple instances of the router.
 // maybe we should make a Route class that stores the routes in a static array.
 // but each route is an instance of the Route class.
 // then the Router class can have a static method to get the routes.
-//Router::get('/notes', [NotesController::class, 'index']);
+//Router::get('/Notes', [NotesController::class, 'index']);
 
