86eff3fe3f9bd925da812780a4567aa596402d39
Cameron Kemshal-Bell <cam@panr.app>
2024-03-02 21:36:49 +1100

    WIP added todo & made autoload reg closure static

--- public/index.php
+++ public/index.php
@@ -6,11 +6,14 @@ error_reporting(E_ALL);
 
 const BASE_PATH = __DIR__.'/../';
 
-// functions are autoloaded in composer.json
+
+// TODO: work out if everything is loaded using composer autoload.
+//  like the functions are in composer.json.
+//  or if we still need spl_autoload register
 require BASE_PATH . 'vendor/autoload.php';
 //require BASE_PATH . 'functions.php';
 
-spl_autoload_register(function ($class) {
+spl_autoload_register(static function ($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
     require base_path("{$class}.php");
