42c8b7be3f4d6c2d8e63d3e8c5df60a666f79724
Cameron Kemshal-Bell <cam@panr.app>
2024-03-03 20:00:27 +1100

    Added a session object

---
+++ app/Core/Session.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Core;
+
+class Session
+{
+    public function __construct()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public function get(string $key, $default = null)
+    {
+        return $_SESSION[$key] ?? $default;
+    }
+
+    public function set(string $key, $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function has(string $key): bool
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    public function remove(string $key): void
+    {
+        unset($_SESSION[$key]);
+    }
+
+    public function clear(): void
+    {
+        session_unset();
+    }
+
+    public function destroy(): void
+    {
+        session_destroy();
+    }
+
+    public function all(): array
+    {
+        return $_SESSION;
+    }
+
+}
