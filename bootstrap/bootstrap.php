ddaaa1f3e158889259c08cef4e3610a62923bbab
Cameron Kemshal-Bell <cam@panr.app>
2024-03-03 16:09:35 +1100

    tidy up of boot ordering in bootstrap.php

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -12,34 +12,24 @@ use App\Services\RouterService;
 // Create the application & container.
 $app = new App();
 
-// Register service providers
+// Register service providers in the correct order
 $app->registerProvider(new EnvService($app));
 $app->registerProvider(new ConfigService($app));
 $app->registerProvider(new DatabaseService($app));
-$app->registerProvider(new RequestService($app));
 $app->registerProvider(new RouterService($app));
-
-//// use a glob pattern to register all service providers
-//foreach (glob(base_path('app/Services/*.php')) as $provider) {
-//    $provider = 'app\\Services\\' . basename($provider, '.php');
-//    $app->registerProvider(new $provider($app));
-//}
+$app->registerProvider(new RequestService($app));
 
 // Boot the Application
 $app->boot();
 
-//dd($app->debugInfo());
-
+// Route the request
 try {
     // Get the request from the container, bound in the service
     $request = $app->resolve(Request::class);
-    $uri = $request->getUri();
-    $method = $request->getMethod();
-
     // Get the router from the container, bound in the service
     $router = $app->resolve(Router::class);
     // Route the request
-    $router->route($uri, $method);
+    $router->route($request);
 } catch (Exception $e) {
     die($e->getMessage());
 }
