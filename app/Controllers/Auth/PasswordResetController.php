a3fb270ea43dfd2ab15c0b8105931285773164ad
Cameron Kemshal-Bell <camkb@icloud.com>
2024-04-08 18:52:42 +1000

    Removed .env from tracking

---
+++ app/Controllers/Auth/PasswordResetController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Core\Controller;
+
+class PasswordResetController extends Controller
+{
+
+}
---
+++ app/Controllers/Auth/RegistrationController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Core\Controller;
+
+class RegistrationController extends Controller
+{
+
+}
