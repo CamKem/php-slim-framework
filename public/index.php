4a65c33af207eb940b1bece667b57c7c7884d0e0
Cam Kemshal-Bell <cam@iterated.tech>
2024-04-29 22:57:03 +1000

    chore: fix composer base config

--- public/index.php
+++ public/index.php
@@ -4,8 +4,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//require __DIR__.'/../vendor/autoload.php';
-
 const BASE_PATH = __DIR__.'/../';
 
 include BASE_PATH . 'bootstrap/functions.php';
@@ -17,6 +15,3 @@ spl_autoload_register(static function ($class) {
 });
 
 require base_path('bootstrap/bootstrap.php');
-
-
-// TODO:  thi is a todo that we can track in the todo list
